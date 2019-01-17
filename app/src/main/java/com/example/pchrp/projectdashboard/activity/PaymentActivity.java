cf491f6775614cad21de5a4bfc5e9d2ce19701cc
PCHRP <fluk.fluk.zuza@gmail.com>
2019-01-17 14:10:33 +0700

    add java file PaymentActivity fragmentNotPay and fragmentPay

---
+++ app/src/main/java/com/example/pchrp/projectdashboard/activity/PaymentActivity.java
@@ -0,0 +1,15 @@
+package com.example.pchrp.projectdashboard.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.pchrp.projectdashboard.R;
+
+public class PaymentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment);
+    }
+}
---
+++ app/src/main/java/com/example/pchrp/projectdashboard/fragment/FragmentNotPay.java
@@ -0,0 +1,64 @@
+package com.example.pchrp.projectdashboard.fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.pchrp.projectdashboard.R;
+
+public class FragmentNotPay extends Fragment {
+    public FragmentNotPay() {
+        super();
+    }
+
+    public static FragmentNotPay newInstance() {
+        FragmentNotPay fragment = new FragmentNotPay();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.frag_not_pay, container, false);
+        initInstances(rootView);
+        return rootView;
+    }
+
+    private void initInstances(View rootView) {
+        // Init 'View' instance(s) with rootView.findViewById here
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+    }
+
+    /*
+     * Save Instance State Here
+     */
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        // Save Instance State here
+    }
+
+    /*
+     * Restore Instance State Here
+     */
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        if (savedInstanceState != null) {
+            // Restore Instance State here
+        }
+    }
+}
