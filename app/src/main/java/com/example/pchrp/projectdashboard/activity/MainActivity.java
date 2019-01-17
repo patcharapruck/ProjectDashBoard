732f80e248c222bb0747e8cba290e9083430b715
PCHRP <fluk.fluk.zuza@gmail.com>
2019-01-18 03:14:03 +0700

    แก้ไข

--- app/src/main/java/com/example/pchrp/projectdashboard/activity/MainActivity.java
+++ app/src/main/java/com/example/pchrp/projectdashboard/activity/MainActivity.java
@@ -4,7 +4,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.pchrp.projectdashboard.R;
-import com.example.pchrp.projectdashboard.fragment.Fragmentmain;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,11 +12,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if (savedInstanceState == null){
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.mainframe, Fragmentmain.newInstance())
-                    .commit();
-
-        }
     }
 }
