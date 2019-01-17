af8567099d8fa5635f87980c80384016e3acde81
PCHRP <fluk.fluk.zuza@gmail.com>
2019-01-18 02:05:52 +0700

    สร้างหน้า main

--- app/src/main/java/com/example/pchrp/projectdashboard/activity/MainActivity.java
+++ app/src/main/java/com/example/pchrp/projectdashboard/activity/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.pchrp.projectdashboard.R;
+import com.example.pchrp.projectdashboard.fragment.Fragmentmain;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null){
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.mainframe, Fragmentmain.newInstance())
+                    .commit();
+
+        }
     }
 }
